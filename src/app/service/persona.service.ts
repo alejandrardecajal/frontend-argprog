a499277d0be5c2bb1cbd6a7f25010d16f65cb3e3
alejandrardecajal <alejandrardecajal@gmail.com>
2022-07-31 18:26:45 -0300

    Primer envio repos separados (Front)

---
+++ src/app/service/persona.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUsername';
+const AUTHORITIES_JEY = 'AuthAuthorities';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PersonaService {
+  URL = 'http://localhost:8080/personas/';
+
+  constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+'traer/perfil');
+
+  }
+}
